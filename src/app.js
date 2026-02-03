c04cc7ee84687fb2680d2b9ecd9418e0bd61c2be
Shivesh Mishra <shiveshmishrajnp@gmail.com>
2026-02-03 20:17:50 +0530

    Fix: Update CORS origin to allow Vercel frontend

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const attendanceRoutes = require("./routes/attendance.routes");
 
 const app = express();
 
-app.use(cors({origin:"https://hrms-backend-zdr7.onrender.com",credentials:true}));
+app.use(cors({origin:"https://hrms-backend-five.vercel.app/",credentials:true}));
 app.use(express.json());
 
 
