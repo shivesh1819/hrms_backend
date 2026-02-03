1369c188c58262690f6e03037abfc7da2abf7fd7
Shivesh Mishra <shiveshmishrajnp@gmail.com>
2026-02-03 20:27:18 +0530

    cors issue

--- src/app.js
+++ src/app.js
@@ -5,8 +5,16 @@ const employeeRoutes = require("./routes/employee.routes");
 const attendanceRoutes = require("./routes/attendance.routes");
 
 const app = express();
-
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://hrms-backend-five.vercel.app", // frontend (Vercel)
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
