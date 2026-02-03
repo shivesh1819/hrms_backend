5e1e267a9eb712cf5e0f70b0cf3d4d990181d054
Shivesh Mishra <shiveshmishrajnp@gmail.com>
2026-02-03 19:32:07 +0530

    Fixed CORS For vercel

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const attendanceRoutes = require("./routes/attendance.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"https://hrms-backend-zdr7.onrender.com",credentials:true}));
 app.use(express.json());
 
 
